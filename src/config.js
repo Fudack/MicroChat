85ec0519d9acbda165bf9897e52813e4833a3265
Fudack <felipe.medina33@inacapmail.cl>
2024-01-08 02:12:57 -0300

    no se solo es bachup

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@
 module.exports = {
     mongoURI: "mongodb+srv://felipe:g6NLCE6trUrYrvM@cluster0.inngonu.mongodb.net/enlacefamiliar?retryWrites=true&w=majority",
     secret: "f801623d61c655055945362009b2b0e7bf102c134c7e0f4e1a35e1efaa2ac953",
-    PORT: 3000,
+    PORT: 8080,
 };
